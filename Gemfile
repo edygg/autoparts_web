ab3dd0cff3690096627ddbc14f0203cadacef396
dacm26 <dacm26@gmail.com>
2014-04-04 19:48:43 +0000

    Devise Implementado

--- Gemfile
+++ Gemfile
@@ -3,6 +3,9 @@ source 'https://rubygems.org'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.0.0'
 
+#Gem for devise
+gem 'devise'
+
 # Use mysql as the database for Active Record
 gem 'mysql2'
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Proyect::Application.routes.draw do
+  devise_for :users
   resources :replacements
 
   resources :cars
 
   resources :types
+
+  resources :home
+
+  root to: "home#index"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
